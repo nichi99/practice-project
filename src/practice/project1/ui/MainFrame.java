40242ba092c913439c7519fa02fa424d887225c8
tylerfilla <tylerfilla@gmail.com>
2014-08-25 21:56:00 -0500

    Added enum for system type and associated dropdown box

--- src/practice/project1/ui/MainFrame.java
+++ src/practice/project1/ui/MainFrame.java
@@ -1,6 +1,11 @@
 package practice.project1.ui;
 
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+import practice.project1.core.EnumType;
 
 public class MainFrame extends JFrame {
 
@@ -15,6 +20,15 @@ public class MainFrame extends JFrame {
 	this.setResizable(false);
 	this.setSize(SIZE_WIDTH, SIZE_HEIGHT);
 	this.setTitle(TITLE);
-    }
+	getContentPane().setLayout(null);
 
+	JLabel lblSelectType = new JLabel("Select type:");
+	lblSelectType.setBounds(10, 11, 374, 14);
+	getContentPane().add(lblSelectType);
+
+	JComboBox comboBox = new JComboBox();
+	comboBox.setModel(new DefaultComboBoxModel(EnumType.values()));
+	comboBox.setBounds(10, 36, 374, 20);
+	getContentPane().add(comboBox);
+    }
 }
